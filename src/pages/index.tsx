70183a3a85630642c3789ced9a006fa09819b5ad
NorbertCseh <norbert.cseh@protonmail.com>
2023-03-28 18:57:58 +0200

    toaster, load state while posting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,8 @@ import { api, type RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { useState } from "react";
-import { LoadingPage } from "~/components/LoadingSpinner";
+import toast from "react-hot-toast";
+import { LoadingPage, LoadingSpinner } from "~/components/LoadingSpinner";
 
 dayjs.extend(relativeTime);
 
@@ -22,6 +23,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later!");
+      }
     },
   });
 
@@ -44,7 +53,14 @@ const CreatePostWizard = () => {
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
       {/* <SignOutButton /> */}
     </div>
   );
